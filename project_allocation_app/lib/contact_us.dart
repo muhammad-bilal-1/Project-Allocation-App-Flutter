3692f62e767caabca8fb5d5bbb4980b2b9229fc8
Fardeenmirza-019 <fardeenmirza90@yahoo.com>
2021-05-25 02:47:08 -0700

    fardeen_mirza
    Fardeen_mirza

---
+++ project_allocation_app/lib/contact_us.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:project_allocation_app/contact_us.dart';
+
+class contact_us extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('image/fardeen.jpeg'),
+                radius: 70.0,
+              ),
+              Text(
+                'FARDEEN MIRZA',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25.0,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'FLUTTER LEARNER',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(
+                width: 400.0,
+                height: 20.0,
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 40),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.phone,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+923401671460',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 30, horizontal: 40),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.black,
+                      size: 30.0,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text('Fardeenmirza5@icloud.com',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ))
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
