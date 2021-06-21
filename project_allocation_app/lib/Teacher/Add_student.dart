204b7912b270712ecdc58ce49e0457118e242bad
hamza027riaz <hamzariaz803@gmail.com>
2021-06-21 23:43:21 +0500

    Update Add_student.dart

--- project_allocation_app/lib/Teacher/Add_student.dart
+++ project_allocation_app/lib/Teacher/Add_student.dart
@@ -1,166 +1,87 @@
-/*
-library contactus;
+import 'dart:async';
+import 'dart:math';
+//import 'package:project_allocation_app/pdf_viewer.dart';
 import 'package:flutter/material.dart';
-///Class for adding contact details/profile details as a complete new page in your flutter app.
-class AddStd extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      appBar: new AppBar(
-        backgroundColor: Colors.blue,
-        title: new Text("Add Student"),
-        /*
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.history),
-            onPressed: () {},
-          ),
-        ],
-
-         */
-      ),
-      backgroundColor: Colors.white,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Add Students Here',
-              style: TextStyle(
-                fontFamily: 'Lobster',
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                fontSize: 35.0,
-              ),
-            ),
-            SizedBox(
-              height: 50.0,
-              width: 350.0,
-              child: Divider(
-                color: Colors.lightBlue,
-                thickness: 1.0,
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: ListTile(
-                leading: Icon(
-                  Icons.account_box,
-                  color: Color(0xFF90CAF9),
-                  size: 30.0,
-                ),
-                title: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.transparent,
-                    hintText: "Add Name here...",
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: ListTile(
-                leading: Icon(
-                  Icons.add_business,
-                  color: Color(0xFF90CAF9),
-                  size: 30.0,
-                ),
-                title: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.transparent,
-                    hintText: "Add Rollno here...",
-                  ),
-                ),
-              ),
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              color: Colors.white,
-              child: ListTile(
-                leading: Icon(
-                  Icons.email_sharp,
-                  color: Color(0xFF90CAF9),
-                  size: 30.0,
-                ),
-                title: TextField(
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: Colors.transparent,
-                    hintText: "Add Email here...",
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-              width: 450.0,
-            ),
-            InkWell(
-              //onTap: () => Get.to(WelcomeScreen()),
-              child: Container(
-                width: 200.0,
-                margin: EdgeInsets.only(left: 15.0, top: 0.0),
-                alignment: Alignment.center,
-                padding: EdgeInsets.all(20), // 15
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Text(
-                  "Add",
-                  style: Theme.of(context)
-                      .textTheme
-                      .button
-                      .copyWith(color: Colors.white, fontSize: 19.0),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
- */
-import 'package:csv/csv.dart';
-import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:path/path.dart' as path;
+//import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:random_string/random_string.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-import 'allCsv.dart';
-import 'loadCsvDataScreen.dart';
+String uri;
+void passurl(final URL) {
+  uri = URL['url'];
+}
 
-class AddStd extends StatelessWidget {
+class AddStd extends StatefulWidget {
+  // const retandup({Key key}) : super(key: key);
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Csv Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Csv Demo'),
+  _retandupState createState() => _retandupState();
+}
+
+class _retandupState extends State<AddStd> {
+  FirebaseStorage storage = FirebaseStorage.instance;
+  Future<void> _upload() async {
+    var rng = new Random();
+    String randomname = "";
+    for (var i = 0; i <= 20; i++) {
+      print(rng.nextInt(100));
+      randomname += rng.nextInt(100).toString();
+    }
+    FilePickerResult result = await FilePicker.platform.pickFiles(
+      allowedExtensions: ['pdf'],
+      type: FileType.custom,
     );
+
+    final String fileName = '${randomname}.pdf';
+    File pdfFile = File(result.files.single.path);
+
+    try {
+      // Uploading the selected image with some custom meta data
+      await storage.ref(fileName).putFile(
+          pdfFile,
+          SettableMetadata(customMetadata: {
+            'uploaded_by': 'Mufi',
+            'description': 'Some description...'
+          }));
+
+      // Refresh the UI
+      setState(() {});
+    } on FirebaseException catch (error) {
+      print(error);
+    }
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  Future<List<Map<String, dynamic>>> _loadPDF() async {
+    List<Map<String, dynamic>> files = [];
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    final ListResult result = await storage.ref().list();
+    final List<Reference> allFiles = result.items;
 
-class _MyHomePageState extends State<MyHomePage> {
+    await Future.forEach<Reference>(allFiles, (file) async {
+      final String fileUrl = await file.getDownloadURL();
+      final FullMetadata fileMeta = await file.getMetadata();
+      files.add({
+        "url": fileUrl,
+        "path": file.fullPath,
+        "uploaded_by": fileMeta.customMetadata['uploaded_by'],
+        "description": fileMeta.customMetadata['description']
+      });
+    });
+
+    return files;
+  }
+
+  Future<void> _delete(String ref) async {
+    await storage.ref(ref).delete();
+    // Rebuild the UI
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -168,114 +89,48 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.blue,
         title: new Text("Add Students"),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MaterialButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => AllCsvFilesScreen()));
-              },
-              child: Container(
-                padding: EdgeInsets.all(20), // 15
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Text(
-                  "Load all Student File",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          children: [
             SizedBox(
-              height: 20.0,
+              height: 250.0,
             ),
-            MaterialButton(
-              onPressed: () {
-                loadCsvFromStorage();
-              },
-              child: Container(
-                padding: EdgeInsets.all(20), // 15
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Text(
-                  "Add Student File form Phone Storage",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+                        textStyle: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold)),
+                    onPressed: () => _upload(),
+                    icon: Icon(Icons.camera),
+                    label: Text('Upload Student File')),
+              ],
             ),
-            SizedBox(
-              height: 20.0,
-            ),
-            /*
-            MaterialButton(
-              onPressed: () {
-                generateCsv();
-              },
-              child: Container(
-                padding: EdgeInsets.all(20), // 15
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Text(
-                  "Load Created Student File",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-
-             */
           ],
         ),
       ),
     );
   }
+}
 
-  generateCsv() async {
-    List<List<String>> data = [
-      ["No.", "Name", "Roll No."],
-      ["1", randomAlpha(3), randomNumeric(3)],
-      ["2", randomAlpha(3), randomNumeric(3)],
-      ["3", randomAlpha(3), randomNumeric(3)]
-    ];
-    String csvData = ListToCsvConverter().convert(data);
-    final String directory = (await getApplicationSupportDirectory()).path;
-    final path = "$directory/csv-${DateTime.now()}.csv";
-    print(path);
-    final File file = File(path);
-    await file.writeAsString(csvData);
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return LoadCsvDataScreen(path: path);
-        },
-      ),
-    );
-  }
+class pdfopen extends StatefulWidget {
+  const pdfopen({Key key}) : super(key: key);
 
-  loadCsvFromStorage() async {
-    FilePickerResult result = await FilePicker.platform.pickFiles(
-      allowedExtensions: ['csv'],
-      type: FileType.custom,
-    );
-    String path = result.files.first.path;
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return LoadCsvDataScreen(path: path);
-        },
-      ),
-    );
+  @override
+  _pdfopenState createState() => _pdfopenState();
+}
+
+class _pdfopenState extends State<pdfopen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            child:
+                SfPdfViewer.network(uri, enableDocumentLinkAnnotation: false)));
   }
 }
