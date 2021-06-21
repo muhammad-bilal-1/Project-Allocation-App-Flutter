5d1bac8be793c480d61669b8be67cbca8b26e6e1
hamza027riaz <hamzariaz803@gmail.com>
2021-06-21 23:23:42 +0500

    Add student pdf file view

--- project_allocation_app/lib/Teacher/View_student.dart
+++ project_allocation_app/lib/Teacher/View_student.dart
@@ -1,68 +1,140 @@
-import 'package:csv/csv.dart';
+import 'dart:async';
+import 'dart:math';
+//import 'package:project_allocation_app/pdf_viewer.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+//import 'package:path/path.dart' as path;
+//import 'package:image_picker/image_picker.dart';
 import 'package:file_picker/file_picker.dart';
-import 'package:random_string/random_string.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-import 'allCsv.dart';
-import 'loadCsvDataScreen.dart';
+String uri;
+void passurl(final URL) {
+  uri = URL['url'];
+}
 
-class ViewStd extends StatelessWidget {
+class ViewStd extends StatefulWidget {
+  // const retandup({Key key}) : super(key: key);
+  final GlobalKey<SfPdfViewerState> _pdfViewerKey = GlobalKey();
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: '',
-      theme: ThemeData(
-        primarySwatch: Colors.orange,
-      ),
-      home: MyHomePage(title: ''),
+  _retandupState createState() => _retandupState();
+}
+
+class _retandupState extends State<ViewStd> {
+  FirebaseStorage storage = FirebaseStorage.instance;
+  Future<void> _upload() async {
+    var rng = new Random();
+    String randomname = "";
+    for (var i = 0; i <= 20; i++) {
+      print(rng.nextInt(100));
+      randomname += rng.nextInt(100).toString();
+    }
+    FilePickerResult result = await FilePicker.platform.pickFiles(
+      allowedExtensions: ['pdf'],
+      type: FileType.custom,
     );
+
+    final String fileName = '${randomname}.pdf';
+    File pdfFile = File(result.files.single.path);
+
+    try {
+      // Uploading the selected image with some custom meta data
+      await storage.ref(fileName).putFile(
+          pdfFile,
+          SettableMetadata(customMetadata: {
+            'uploaded_by': 'List',
+            'description': 'Some description...'
+          }));
+
+      // Refresh the UI
+      setState(() {});
+    } on FirebaseException catch (error) {
+      print(error);
+    }
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  Future<List<Map<String, dynamic>>> _loadPDF() async {
+    List<Map<String, dynamic>> files = [];
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    final ListResult result = await storage.ref().list();
+    final List<Reference> allFiles = result.items;
 
-class _MyHomePageState extends State<MyHomePage> {
+    await Future.forEach<Reference>(allFiles, (file) async {
+      final String fileUrl = await file.getDownloadURL();
+      final FullMetadata fileMeta = await file.getMetadata();
+      files.add({
+        "url": fileUrl,
+        "path": file.fullPath,
+        "uploaded_by": fileMeta.customMetadata['uploaded_by'],
+        "description": fileMeta.customMetadata['description']
+      });
+    });
+
+    return files;
+  }
+
+  Future<void> _delete(String ref) async {
+    await storage.ref(ref).delete();
+    // Rebuild the UI
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
         backgroundColor: Colors.orange,
-        title: new Text(
-          "View students",
-          style: TextStyle(
-            color: Colors.white,
-          ),
-        ),
+        title: new Text("View Students"),
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(20),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MaterialButton(
-              onPressed: () {
-                generateCsv();
-              },
-              child: Container(
-                padding: EdgeInsets.all(20), // 15
-                decoration: BoxDecoration(
-                  color: Colors.orange,
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Text(
-                  "View Studnet File",
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
+          children: [
+            Expanded(
+              child: FutureBuilder(
+                future: _loadPDF(),
+                builder: (context,
+                    AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      itemBuilder: (context, index) {
+                        final PDF = snapshot.data[index];
+                        return Card(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          child: ListTile(
+                            dense: false,
+                            leading: Image.asset('assets/img/12.png',
+                                width: 50.0, height: 50.0),
+                            title: Text(PDF['uploaded_by']),
+                            subtitle: Text(PDF['description']),
+                            trailing: IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  passurl(snapshot.data[index]);
+                                  Navigator.of(context).pushReplacement(
+                                      MaterialPageRoute(
+                                          builder: (context) => pdfopen()));
+                                });
+                              },
+                              icon: Icon(
+                                Icons.table_view_sharp,
+                                color: Colors.orangeAccent,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
               ),
             ),
           ],
@@ -70,32 +142,21 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
+}
 
-  generateCsv() async {
-    List<List<String>> data = [
-      ["No.", "Name", "Roll No."],
-      ["1", "Aqsa Tariq", "12"],
-      ["2", "Adeela Shahzadi", "03"],
-      ["3", "Mufassil Hussain", "49"],
-      ["4", "Hamza Riaz", "26"],
-      ["5", "Bilal Jutt", "51"],
-      ["6", "Fardeen Mirza", "19"],
-      ["7", "Ahmed Junaid", "04"],
-      ["8", "Hamaz Ali", "24"],
-      ["9", "Ahtisham Akbar", "29"],
-    ];
-    String csvData = ListToCsvConverter().convert(data);
-    final String directory = (await getApplicationSupportDirectory()).path;
-    final path = "$directory/csv-${DateTime.now()}.csv";
-    print(path);
-    final File file = File(path);
-    await file.writeAsString(csvData);
-    Navigator.of(context).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return LoadCsvDataScreen(path: path);
-        },
-      ),
-    );
+class pdfopen extends StatefulWidget {
+  const pdfopen({Key key}) : super(key: key);
+
+  @override
+  _pdfopenState createState() => _pdfopenState();
+}
+
+class _pdfopenState extends State<pdfopen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            child:
+                SfPdfViewer.network(uri, enableDocumentLinkAnnotation: false)));
   }
 }
